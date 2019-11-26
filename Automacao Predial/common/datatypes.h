aaa1a78a62c37ab809cef1ebf3386807a97b5888
Lucas Cardoso de Medeiros <lucascarmed@gmail.com>
2019-11-26 09:04:37 -0300

    Update datatypes.h

--- Automacao Predial/common/datatypes.h
+++ Automacao Predial/common/datatypes.h
@@ -11,6 +11,7 @@
     typedef int                 int32;
 #endif
 
+//Macro compilação Keil Vision
 #ifdef __C51__
     typedef unsigned char       uint8;
     typedef unsigned short      uint16;
@@ -19,6 +20,17 @@
     typedef char                int8;
     typedef short               int16;
     typedef long                int32;
+#endif
+
+//Macro compilação Renesas
+#ifdef __RENESAS__
+    typedef char                int8;
+    typedef short               int16;
+    typedef int                 int32;
+
+    typedef unsigned char       uint8;
+    typedef unsigned short      uint16;
+    typedef unsigned int        uint32;
 #endif
 
 typedef uint8   boolean;
